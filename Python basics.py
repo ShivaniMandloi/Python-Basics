d5bc6fe3214177d104356462c72984094aff739b
ShivaniMandloi <70172119+ShivaniMandloi@users.noreply.github.com>
2023-08-19 22:04:17 +0530

    Create Python basics.py

---
+++ Python basics.py
@@ -0,0 +1,8 @@
+# Functions + Module = Standard Library
+# These are some standard Library examples
+
+# 1. os provides a platform-independent way to interact with your underlying operating system
+from os import getcwd()
+xyz = getcwd()  # this is called invoking the function
+
+
